a55f741fe7685d866ae1b229192df45c6df28da9
NABrown843 <87168340+NABrown843@users.noreply.github.com>
2022-03-20 17:18:56 -0400

    GCI - Two Pointer Practice

---
+++ G_RemoveAllInstancesOfKey.py
@@ -0,0 +1,8 @@
+def remove_element(arr, key):
+  nextElement = 0  # index of the next element which is not 'key'
+  for i in range(len(arr)):
+    if arr[i] != key:
+      arr[nextElement] = arr[i]
+      nextElement += 1
+
+  return nextElement
---
+++ G_RemoveDuplicates.py
@@ -0,0 +1,9 @@
+def remove_duplicates(arr):
+  non_dupe = 1
+  i = 0
+  while i < len(arr):
+    if arr[non_dupe - 1] != arr[i]:
+      arr[non_dupe] = arr[i]
+      non_dupe += 1
+    i += 1
+  return non_dupe
