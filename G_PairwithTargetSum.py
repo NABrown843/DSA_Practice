b1393958b83b0910453b9ed1dad80ec8ee364d70
NABrown843 <87168340+NABrown843@users.noreply.github.com>
2022-03-28 22:44:44 -0400

    GTCI - Two Pointer Practice

--- G_PairwithTargetSum.py
+++ G_PairwithTargetSum.py
@@ -1,13 +1,13 @@
 def pair_with_targetsum(arr, target_sum):
-  left = 0
-  right = len(arr) - 1
-  
-  while left < right:
-    curr_sum = arr[left] + arr[right]
-    if curr_sum == target_sum:
-      return [left, right]
-    if curr_sum > target_sum:
-      right -= 1
-    else:
-      left += 1
-  return [-1, -1]
+    left = 0
+    right = len(arr) - 1
+
+    while left < right:
+        curr_sum = arr[left] + arr[right]
+        if curr_sum == target_sum:
+            return [left, right]
+        if curr_sum > target_sum:
+            right -= 1
+        else:
+            left += 1
+    return [-1, -1]
--- G_SquaringaSortedArray.py
+++ G_SquaringaSortedArray.py
@@ -1,16 +1,16 @@
 def make_squares(arr):
-  n = len(arr)
-  squares = [0 for x in range(n)]
-  highest_square_index = n -1
-  left, right = 0, n-1
-  while left <= right:
-    left_square = arr[left]**2
-    right_square = arr[right]**2
-    if left_square > right_square:
-      squares[highest_square_index] = left_square
-      left += 1
-    else:
-      squares[highest_square_index] = right_square
-      right -= 1
-    highest_square_index -= 1
-  return squares
+    n = len(arr)
+    squares = [0 for x in range(n)]
+    highest_square_index = n - 1
+    left, right = 0, n - 1
+    while left <= right:
+        left_square = arr[left] ** 2
+        right_square = arr[right] ** 2
+        if left_square > right_square:
+            squares[highest_square_index] = left_square
+            left += 1
+        else:
+            squares[highest_square_index] = right_square
+            right -= 1
+        highest_square_index -= 1
+    return squares
