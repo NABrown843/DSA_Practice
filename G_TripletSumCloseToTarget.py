0b3019b6552ac2c73c671c10f89e079051199dd9
NABrown843 <87168340+NABrown843@users.noreply.github.com>
2022-03-30 17:45:03 -0400

    GTCI - Two Pointer Practice

--- G_TripletSumCloseToTarget.py
+++ G_TripletSumCloseToTarget.py
@@ -1,30 +1,31 @@
 import sys
 
+
 def triplet_sum_close_to_target(arr, target_sum):
-  # TODO: Write your code here
-  arr.sort()
-  min_sum = sys.maxsize
-  min_diff = sys.maxsize
+    # TODO: Write your code here
+    arr.sort()
+    min_sum = sys.maxsize
+    min_diff = sys.maxsize
 
-  for i in range(len(arr)):
-    left = i + 1
-    right = len(arr) - 1
+    for i in range(len(arr)):
+        left = i + 1
+        right = len(arr) - 1
 
-    while left < right:
-      curr_sum = arr[i] + arr[left] + arr[right]
-      curr_diff = abs(target_sum - curr_sum)
+        while left < right:
+            curr_sum = arr[i] + arr[left] + arr[right]
+            curr_diff = abs(target_sum - curr_sum)
 
-      if target_sum == curr_sum:
-        break
-      elif target_sum > curr_sum:
-        left += 1
-      else:
-        right -= 1
+            if target_sum == curr_sum:
+                break
+            elif target_sum > curr_sum:
+                left += 1
+            else:
+                right -= 1
 
-      if curr_diff < min_diff:
-        min_diff = curr_diff
-        min_sum = curr_sum
-      elif curr_diff == min_diff:
-        min_sum = min(min_sum, curr_sum)
+            if curr_diff < min_diff:
+                min_diff = curr_diff
+                min_sum = curr_sum
+            elif curr_diff == min_diff:
+                min_sum = min(min_sum, curr_sum)
 
-  return min_sum
+    return min_sum
--- G_TripletsWithSmallerSum.py
+++ G_TripletsWithSmallerSum.py
@@ -1,18 +1,19 @@
 def triplet_with_smaller_sum(arr, target):
-  count = 0
-  arr.sort()
-  # TODO: Write your code here
-  for i in range(len(arr) - 2):
-    left = i + 1
-    right = len(arr) - 1
-    while left < right:
-      curr_sum = arr[i] + arr[left] + arr[right]
-      if curr_sum < target:
-        count += 1
-        left += 1
-      else:
-        right -= 1
-  
-  return count
+    count = 0
+    arr.sort()
+    # TODO: Write your code here
+    for i in range(len(arr) - 2):
+        left = i + 1
+        right = len(arr) - 1
+        while left < right:
+            curr_sum = arr[i] + arr[left] + arr[right]
+            if curr_sum < target:
+                count += 1
+                left += 1
+            else:
+                right -= 1
+
+    return count
+
 
 print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
